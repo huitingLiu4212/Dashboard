42cc66e07adfefec5139807eeb3ff18dd5e2f1af
huiting.liu <421241578@qq.com>
2017-05-17 18:46:18 +0800

    view change，add table

--- scripts/view.js
+++ scripts/view.js
@@ -1,4 +1,68 @@
 $( function() {
+	$('#toggerbtn').on('click',function(){
+		$('#schContent').slideToggle();
+	});
 
+    var data = [
+        {
+            "customer":   "NEWCO",
+            "location":   "SMF1 - Nodes only",
+            "shelfLocation": "A3",
+            "partNumber": "12341234",
+            "type":     "Cable",
+            "description":  "Test",
+			"quantity":  "1"
+        },
+        {
+            "customer":   "NEWCO2",
+            "location":   "SMF2 - Nodes only",
+            "shelfLocation": "A3",
+            "partNumber": "123412345",
+            "type":     "Cable",
+            "description":  "Testaaa",
+			"quantity":  "2"
+        },
+        {
+            "customer":   "NEWCO2",
+            "location":   "SMF2 - Nodes only",
+            "shelfLocation": "A3",
+            "partNumber": "123412345",
+            "type":     "Cable",
+            "description":  "Testaaa",
+			"quantity":  "2"
+        },
+        {
+            "customer":   "NEWCO2",
+            "location":   "SMF2 - Nodes only",
+            "shelfLocation": "A3",
+            "partNumber": "123412345",
+            "type":     "Cable",
+            "description":  "Testaaa",
+			"quantity":  "2"
+        },
+        {
+            "customer":   "NEWCO2",
+            "location":   "SMF2 - Nodes only",
+            "shelfLocation": "A3",
+            "partNumber": "123412345",
+            "type":     "Cable",
+            "description":  "Testaaa",
+			"quantity":  "2"
+        }
+    ];
 
+	$('#sortable').DataTable({
+		data:data,
+		fixedHeader:true,
+		colReorder:true,
+		columns:[
+			{data:"customer"},
+			{data:"location"},
+			{data:"shelfLocation"},
+			{data:"partNumber"},
+			{data:"type"},
+			{data:"description"},
+			{data:"quantity"}
+		]
+	})
  });
