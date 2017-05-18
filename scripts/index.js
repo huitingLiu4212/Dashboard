4dcfac1d97531eda0392356528ffa72ee0fd215f
huiting.liu <421241578@qq.com>
2017-05-18 17:25:08 +0800

    creat nav

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,14 @@
 $( function() {
 
-    $( "#sortable" ).sortable();
+    var sortable = $( "#sortable" ).sortable({ 
+    	cancel: ".pannel-content",
+    	revert: true,
+    	change: function(event, ui){
+    		console.dir(event);
+    		console.dir(ui);
+    		console.dir($( "#sortable" ).sortable('toArray'));
+    	}
+    });
 
     $('#dialog *[name=close]').on('click',function(){
     	$('#mark').hide();
